552d9e1c71ec706b22d9f733474817581a82a2f0
286101083qq <286101083@qq.com>
2025-06-12 16:26:06 +0800

    Add files via upload

---
+++ ooonlab-site /pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Welcome to Ooon Lab</h1>
+      <p>This is your NFC Ring Project landing page.</p>
+    </div>
+  )
+}
